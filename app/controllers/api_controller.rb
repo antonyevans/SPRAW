c0d5b53c2488af5dfe1160b10f1d82425334e2ec
Antony Evans <antonyevans@gmail.com>
2012-03-20 11:38:42 -0700

    created API controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,5 @@
+class ApiController < ApplicationController
+  def parse
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 StanfordNLP::Application.routes.draw do
+  get "api/parse"
+
   get "browse/test"
 
   # The priority is based upon order of creation:
