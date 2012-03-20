e44361f6c72f2d1850099b30c4e53ca5124055e7
Antony Evans <antonyevans@gmail.com>
2012-03-20 11:37:30 -0700

    added parse info to controller

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,5 +1,12 @@
 class BrowseController < ApplicationController
   def test
+    unless params[:input].nil? || params[:input] == ""
+      @outputstring = $parser.getparsedstring(params[:input])
+      @outputwordtree = "Coming Soon!"
+    else
+      @outputstring = "[            ]"
+      @outputwordtree = "[            ]"
+    end
   end
 
 end
