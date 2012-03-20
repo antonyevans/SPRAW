a257a09fd2315d6c054b8f7f3c3284db6633b400
Antony Evans <antonyevans@gmail.com>
2012-03-19 19:06:47 -0700

    reset app

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-StanfordNLP::Application.config.secret_token = '120490bb61a560ec6fee1726fce0a3e37dfd83ff9adfe226cb415991e361dcfd80a9fb7a23592e9e33cbdb32fe64f11599930ea2b4426e599d431420e26d57b3'
